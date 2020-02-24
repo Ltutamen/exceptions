c155e10667ec90b7974efff776bc261b1c7087b0
luxTutamen <kovez@protonmail.com>
2020-02-24 15:47:11 +0200

    initial commit

---
+++ src/ua/axiom/Exceptionr.java
@@ -0,0 +1,415 @@
+package ua.axiom;
+
+import java.lang.Exception;
+
+public class Exceptionr {
+    public static void e1() {
+        try {
+        } catch (Throwable t) {}
+    }
+
+    public static void e2() {
+        // Error - потомок Throwable
+        throw new Error();
+    }
+
+    public static void e3() {
+        throw null;
+    }
+
+    public static void e4() {
+        Error ref = new Error(); // создаем экземпляр
+        throw ref;               // "бросаем" его
+    }
+
+    public static void e5() {
+        double d = sqr(10.0);  // sqr - навсегда "повиснет", и
+        System.out.println(d); // d - НИКОГДА НИЧЕГО НЕ БУДЕТ ПРИСВОЕНО!
+    }
+
+    public static double sqr(double arg) {
+        while (true); // Вот тут мы на века "повисли"
+    }
+
+    public static double sqr2(double arg) {// согласно объявлению метода ты должен вернуть double
+        long time = System.currentTimeMillis();
+        if (time % 2 == 0) {
+            return arg * arg;             // ок, вот твой double
+        } else if (time % 2 == 1) {
+            {
+                while (true) ;                 // не, я решил "повиснуть"
+            }
+        } else {
+                throw new RuntimeException(); // или бросить исключение
+        }
+    }
+
+
+    public static int area(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Negative sizes: w = " + width + ", h = " + height);
+        }
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        System.err.println("#1.in");
+        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println("#1.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void f() {
+        System.err.println(".   #2.in");
+        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   #2.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void g() {
+        System.err.println(".   .   #3.in");
+        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void h() {
+        System.err.println(".   .   .   #4.in");
+        if (true) {
+            System.err.println(".   .   .   #4.THROW");
+            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+        }
+        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void mma() {
+        System.err.println("#1.in");
+        try {
+            f(); // создаем фрейм, помещаем в стек, передаем в него управление
+        } catch (Error e) { // "перехватили" "летящее" исключение
+            System.err.println("#1.CATCH");  // и работаем
+        }
+        System.err.println("#1.out");  // работаем дальше
+    }
+
+    public static void f1() {
+        System.err.println(".   #2.in");
+        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   #2.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void g2() {
+        System.err.println(".   .   #3.in");
+        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void h3() {
+        System.err.println(".   .   .   #4.in");
+        if (true) {
+            System.err.println(".   .   .   #4.THROW");
+            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+        }
+        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void mmma() {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new RuntimeException();}
+            System.err.print(" 1");
+        }
+        catch (java.lang.Exception e) { // catch по Exception ПЕРЕХВАТЫВАЕТ RuntimeException
+            System.err.print(" 2");
+        }
+        System.err.println(" 3");
+    }
+
+    public static void mammamain() {
+        try {
+            throw new RuntimeException();
+        } catch (java.lang.Exception e) {
+            if (e instanceof RuntimeException) {
+                RuntimeException re = (RuntimeException) e;
+                System.err.print("Это RuntimeException на самом деле!!!");
+            } else {
+                System.err.print("В каком смысле не RuntimeException???");
+            }
+        }
+    }
+
+    public static void maidfdn() throws Exception { // пока игнорируйте 'throws'
+        try {
+            System.err.print(" 0");
+            if (true) {throw new Exception();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) {
+            System.err.print(" 2");
+        }
+        System.err.print(" 3");
+    }
+
+    public static void dsvsd() {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new Error();}
+            System.err.print(" 1");
+        } catch (Exception e) {
+            System.err.print(" 2");
+        }
+        System.err.print(" 3");
+    }
+
+    public static void vsvs() {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new RuntimeException();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) {     // перехватили RuntimeException
+            System.err.print(" 2");
+            if (true) {throw new Error();} // но бросили Error
+        }
+        System.err.println(" 3");          // пропускаем - уже летит Error
+    }
+
+    public static void klkll() {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new RuntimeException();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) { // перехватили RuntimeException
+            System.err.print(" 2");
+            if (true) {throw e;}       // и бросили ВТОРОЙ раз ЕГО ЖЕ
+        }
+        System.err.println(" 3");      // пропускаем - опять летит RuntimeException
+    }
+
+    public static void csdc() {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new RuntimeException();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) {     // перехватили RuntimeException
+            System.err.print(" 2");
+            if (true) {throw new Error();} // и бросили новый Error
+        } catch (Error e) { // хотя есть cath по Error "ниже", но мы в него не попадаем
+            System.err.print(" 3");
+        }
+        System.err.println(" 4");
+    }
+
+    public static void mainsfsd() {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new RuntimeException();}
+            System.err.print(" 1");
+        } catch (RuntimeException e) { // перехватили RuntimeException
+            System.err.print(" 2.1");
+            try {
+                System.err.print(" 2.2");
+                if (true) {throw new Error();} // и бросили новый Error
+                System.err.print(" 2.3");
+            } catch (Throwable t) {            // перехватили Error
+                System.err.print(" 2.4");
+            }
+            System.err.print(" 2.5");
+        } catch (Error e) { // хотя есть cath по Error "ниже", но мы в него не попадаем
+            System.err.print(" 3");
+        }
+        System.err.println(" 4");
+    }
+
+    public static void mfwdain() {
+        try {
+        } catch (Exception e) {
+        //  } catch (RuntimeException e) {
+        }
+    }
+
+    public static void sfs() {
+        try {
+            throw new Exception();
+        } catch (RuntimeException e) {
+            System.err.println("catch RuntimeException");
+        } catch (Exception e) {
+            System.err.println("catch Exception");
+        } catch (Throwable e) {
+            System.err.println("catch Throwable");
+        }
+        System.err.println("next statement");
+    }
+
+    public static void vsv() {
+        try {
+            System.err.println("try");
+        } finally {
+            System.err.println("finally");
+        }
+    }
+
+    public static void sdcs() {
+        try {
+            throw new RuntimeException();
+        } finally {
+            System.err.println("finally");
+        }
+    }
+
+    public static void sfsvsf() {
+        try {
+            return;
+        } finally {
+            System.err.println("finally");
+        }
+    }
+
+    public static void sdfs() {
+        try {
+            System.exit(42);
+        } finally {
+            System.err.println("finally");
+        }
+    }
+
+    public static void madwedin() {
+        System.err.println(f44());
+    }
+
+    public static int f44() {
+        try {
+            return 0;
+        } finally {
+            return 1;
+        }
+    }
+
+    public static void dfvdfv() {
+        System.err.println(f66());
+    }
+
+    public static int f66() {
+        try {
+            throw new RuntimeException();
+        } finally {
+            return 1;
+        }
+    }
+
+    public static void msfsdf() {
+        System.err.println(f11());
+    }
+
+    public static int f11() {
+        long rnd = System.currentTimeMillis();
+        boolean finished = false;
+        try {
+            if (rnd % 3 == 0) {
+                throw new Error();
+            } else if (rnd % 3 == 1) {
+                throw new RuntimeException();
+            } else {
+                // nothing
+            }
+            finished = true;
+        } finally {
+            if (finished) {
+                // не было исключений
+            } else {
+                // было исключение, но какое?
+            }
+        }
+
+        return 1;
+    }
+
+    public static void massa() {
+        try {
+            System.err.print(" 0");
+            // nothing
+            System.err.print(" 1");
+        } catch(Error e) {
+            System.err.print(" 2");
+        } finally {
+            System.err.print(" 3");
+        }
+        System.err.print(" 4");
+    }
+
+    public static void vdfv() {
+        try {
+            System.err.print(" 0");
+            if (true) {throw new Error();}
+            System.err.print(" 1");
+        } catch(Error e) {
+            System.err.print(" 2");
+        } finally {
+            System.err.print(" 3");
+        }
+        System.err.print(" 4");
+    }
+
+    public static void ffvdv() {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                // НИЧЕГО
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - нет исключения
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // заходим - выполнение в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // НЕ заходим - нет исключения
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // заходим - выполнение в норме
+    }
+
+    public static void dbvdf() {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {throw new Exception();}
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // не заходим - выполнение НЕ в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // ЗАХОДИМ - есть подходящее исключение
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // заходим - выполнение УЖЕ в норме
+    }
+
+    public static void dfvdf() {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {throw new Error();}
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // НЕ заходим - выполнение НЕ в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // не заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // не заходим - выполнение НЕ в норме
+    }
+
+
+
+
+
+}
---
+++ src/ua/axiom/Main.java
@@ -0,0 +1,8 @@
+package ua.axiom;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int i=0;
+    }
+}
